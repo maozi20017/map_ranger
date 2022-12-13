2d33eb3baf592523a6914ed5790ca05f700f94f1
maozi20017 <82589961+maozi20017@users.noreply.github.com>
2022-12-13 10:40:46 +0800

    Initial commit

---
+++ map_ranger.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/go-vgo/robotgo"
+	gohook "github.com/robotn/gohook"
+)
+
+var (
+	meter, thebase float64
+	x1, y1, x2, y2 int
+)
+
+func main() {
+	fmt.Println("先設定一格長度")
+	thebase = thefirst()
+	fmt.Printf("%d", int(thebase))
+	fmt.Println("輸入比例尺")
+	fmt.Scanln(&meter)
+	for {
+		getrange()
+	}
+}
+
+func thefirst() float64 {
+	fmt.Println("按下滑鼠中鍵以設定初始起點")
+	ok := gohook.AddMouse("center") //原本是robotgo.AddEvent 已改成 gohook.AddEvent
+	if ok {
+		x1, y1 = robotgo.GetMousePos()
+		fmt.Println(("按下滑鼠中鍵以設定初始終點"))
+	}
+	ok = gohook.AddMouse("center")
+	if ok {
+		x2, y2 = robotgo.GetMousePos()
+	}
+	return math.Sqrt(math.Pow(float64(x2-x1), 2) + math.Pow(float64(y2-y1), 2))
+}
+
+func getrange() {
+
+	fmt.Println("按下滑鼠中鍵以設定起點")
+	ok := gohook.AddMouse("center") //原本是robotgo.AddEvent 已改成 gohook.AddEvent
+	if ok {
+		x1, y1 = robotgo.GetMousePos()
+		fmt.Println(("按下滑鼠中鍵以設定終點"))
+	}
+	ok = gohook.AddMouse("center")
+	if ok {
+		x2, y2 = robotgo.GetMousePos()
+		therange := int(math.Sqrt((math.Pow(float64(x2-x1), 2) + math.Pow(float64(y2-y1), 2))) / thebase * meter)
+		fmt.Printf("%d\n", int(math.Sqrt((math.Pow(float64(x2-x1), 2) + math.Pow(float64(y2-y1), 2)))))
+		fmt.Printf("距離 :%d\n", therange)
+	}
+}
