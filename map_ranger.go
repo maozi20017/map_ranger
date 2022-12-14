0ceaf25f36407daf43d4d6486a17e713603dfc57
maozi20017 <82589961+maozi20017@users.noreply.github.com>
2022-12-14 18:24:25 +0800

    稍微封裝了一下

--- map_ranger.go
+++ map_ranger.go
@@ -18,7 +18,6 @@ func main() {
 	fmt.Scanln(&meter)
 	fmt.Println("設定一格長度")
 	baselength = firststep()
-	fmt.Printf("%d", int(baselength))
 	for {
 		getrange()
 	}
@@ -26,30 +25,26 @@ func main() {
 
 func firststep() float64 {
 	fmt.Println("按下滑鼠中鍵以設定初始起點")
-	ok := gohook.AddMouse("center") //原本是robotgo.AddEvent 已改成 gohook.AddEvent
-	if ok {
-		x1, y1 = robotgo.GetMousePos()
-		fmt.Println(("按下滑鼠中鍵以設定初始終點"))
-	}
-	ok = gohook.AddMouse("center")
-	if ok {
-		x2, y2 = robotgo.GetMousePos()
-	}
+	x1, y1 = togetmousepos()
+	fmt.Println(("按下滑鼠中鍵以設定初始終點"))
+	x2, y2 = togetmousepos()
 	return math.Hypot(math.Abs(float64(x2-x1)), math.Abs(float64(y2-y1)))
 }
 
 func getrange() {
 
 	fmt.Println("按下滑鼠中鍵以設定起點")
+	x1, y1 = togetmousepos()
+	fmt.Println(("按下滑鼠中鍵以設定終點"))
+	x2, y2 = togetmousepos()
+	therange := int(math.Hypot(math.Abs(float64(x2-x1)), math.Abs(float64(y2-y1))) / baselength * meter)
+	fmt.Printf("距離:%d\n", therange)
+}
+
+func togetmousepos() (x int, y int) {
 	ok := gohook.AddMouse("center") //原本是robotgo.AddEvent 已改成 gohook.AddEvent
 	if ok {
 		x1, y1 = robotgo.GetMousePos()
-		fmt.Println(("按下滑鼠中鍵以設定終點"))
 	}
-	ok = gohook.AddMouse("center")
-	if ok {
-		x2, y2 = robotgo.GetMousePos()
-		therange := int(math.Hypot(math.Abs(float64(x2-x1)), math.Abs(float64(y2-y1))) / baselength * meter)
-		fmt.Printf("距離:%d\n", therange)
-	}
+	return x, y
 }
