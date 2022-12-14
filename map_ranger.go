3c30e5eecd2ab5413d8e955d63f62881ca199827
maozi20017 <82589961+maozi20017@users.noreply.github.com>
2022-12-14 17:49:56 +0800

    改了一點bug

--- map_ranger.go
+++ map_ranger.go
@@ -9,22 +9,22 @@ import (
 )
 
 var (
-	meter, thebase float64
-	x1, y1, x2, y2 int
+	meter, baselength float64
+	x1, y1, x2, y2    int
 )
 
 func main() {
 	fmt.Println("輸入比例尺")
 	fmt.Scanln(&meter)
 	fmt.Println("設定一格長度")
-	thebase = thefirst()
-	fmt.Printf("%d", int(thebase))
+	baselength = firststep()
+	fmt.Printf("%d", int(baselength))
 	for {
 		getrange()
 	}
 }
 
-func thefirst() float64 {
+func firststep() float64 {
 	fmt.Println("按下滑鼠中鍵以設定初始起點")
 	ok := gohook.AddMouse("center") //原本是robotgo.AddEvent 已改成 gohook.AddEvent
 	if ok {
@@ -49,8 +49,7 @@ func getrange() {
 	ok = gohook.AddMouse("center")
 	if ok {
 		x2, y2 = robotgo.GetMousePos()
-		therange := int(math.Sqrt((math.Pow(float64(x2-x1), 2) + math.Pow(float64(y2-y1), 2))) / thebase * meter)
-		fmt.Printf("%d\n", int(math.Sqrt((math.Pow(float64(x2-x1), 2) + math.Pow(float64(y2-y1), 2)))))
+		therange := int(math.Sqrt((math.Pow(math.Abs(float64(x2-x1)), 2) + math.Pow(math.Abs(float64(y2-y1)), 2))) / baselength * meter)
 		fmt.Printf("距離:%d\n", therange)
 	}
 }
