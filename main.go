9646a38b0eb38b16d39b82d8d41a19e3a0bd1475
毛子 <smallg20017@gmail.com>
2023-06-07 04:22:18 +0800

    update

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ var (
 )
 
 func main() {
-Here:
 	fmt.Println("輸入比例尺")
 	fmt.Scanln(&meter)
 	fmt.Println("設定一格長度")
@@ -24,6 +23,7 @@ Here:
 	}
 }
 
+// 設定初始步驟，用於測量起點和終點之間的基準距離
 func firststep() float64 {
 	fmt.Println("按下滑鼠中鍵以設定初始起點")
 	x1, y1 = togetmousepos()
@@ -32,6 +32,7 @@ func firststep() float64 {
 	return math.Hypot(math.Abs(float64(x2-x1)), math.Abs(float64(y2-y1)))
 }
 
+// 獲取起點和終點之間的距離
 func getrange() {
 	fmt.Println("按下滑鼠中鍵以設定起點")
 	x1, y1 = togetmousepos()
@@ -41,11 +42,11 @@ func getrange() {
 	fmt.Printf("距離:%d\n", therange)
 }
 
+// 獲取滑鼠座標位置
 func togetmousepos() (x int, y int) {
 	ok := gohook.AddMouse("center") //原本是robotgo.AddEvent 已改成 gohook.AddEvent
 	if ok {
 		x, y = robotgo.GetMousePos()
 	}
-	goto Here
 	return x, y
 }
